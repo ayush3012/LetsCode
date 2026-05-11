50ceefd1777a49d63af631cd94989485d05e0b95
ayushkarna <k.ayush3012@gmail.com>
2026-05-11 10:50:55 +0530

    Added another method for StockBuySell.It can store index of buy and sell

--- DailyCheck/com/MyPack/Array/StockBuySell.java
+++ DailyCheck/com/MyPack/Array/StockBuySell.java
@@ -16,11 +16,39 @@ public class StockBuySell {
 		System.out.println("max profit "+max);
 	}
 
+	public static void StockBuyNSell_method2(int[] arr)
+	{
+		int buy=0;
+		int buyind=0;
+		int sell=0;
+		int profit=0;
+		int maxprofit=0;
+		int minsofar=arr[0];
+		for(int i=1;i<arr.length;i++)
+		{
+			profit=arr[i]-minsofar;
+			if(profit>maxprofit)
+			{
+				maxprofit=profit;
+				buyind=buy;
+				sell=i;
+			}
+			if(arr[i]<minsofar)
+			{
+				minsofar=arr[i];
+				buy=i;
+			}
+		}
+		System.out.println("Buy at "+buy+" sell at "+sell+" with profit "+maxprofit);
+	}
+
 	public static void main(String[] args) {
 		
 		int price[] = { 100, 180, 260, 310, 40, 535, 695 };
 		
-		StockBuyNSell(price);	
+		StockBuyNSell(price);
+
+		StockBuyNSell_method2(price);
 
 	}
 
