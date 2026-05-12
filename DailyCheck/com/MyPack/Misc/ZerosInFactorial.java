8bd992b02c5d7cfb9464af8166ee20ba9fb8d3c5
ayushkarna <k.ayush3012@gmail.com>
2026-05-12 11:05:45 +0530

    count zero in factorial

--- DailyCheck/com/MyPack/Misc/ZerosInFactorial.java
+++ DailyCheck/com/MyPack/Misc/ZerosInFactorial.java
@@ -13,6 +13,17 @@ public class ZerosInFactorial {
 		}
 		System.out.println("total zeros "+count);
 	}
+
+	public static void CountZero_method2(double n)
+	{
+		int count=0;
+		while(n>0)
+		{
+			n=Math.floor(n/5);
+			count+=n;
+		}
+		System.out.println(n);
+	}
 	public static void main(String[] args) {
 		
 		CountZero(25);
