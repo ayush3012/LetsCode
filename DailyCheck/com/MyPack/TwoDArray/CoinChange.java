9ecaa78a57d3012943c1c2b389a727c0cb4c18f1
ayushkarna <k.ayush3012@gmail.com>
2026-05-14 10:28:20 +0530

    coin change problem with DP

--- DailyCheck/com/MyPack/TwoDArray/CoinChange.java
+++ DailyCheck/com/MyPack/TwoDArray/CoinChange.java
@@ -29,6 +29,36 @@ public class CoinChange {
 			combination.remove(combination.size()-1);
 		}
 	}
+
+	/*
+	* 1. Create array of list . dp[i]-> store all combination that make sum
+	* 2. Initialize each dp[i] with an empty arraylist. After this dp[0], dp[1] ...will exists and can
+	*    store combination.
+	* 3. outer for loop -> pick one coin at a time
+	* 4. middle for loop try to build every amount using that coin
+	* 5. extend every existing combination by adding that coin
+	* */
+	static void CoinChange_Method(int[] arr,int sum)
+	{
+		List<List<Integer>>[] dp=new ArrayList[sum+1];
+		for(int i=0;i<=sum;i++)
+			dp[i]=new ArrayList<>();
+		dp[0].add(new ArrayList<>());
+		for(int a:arr)
+		{
+			for(int i=a;i<=sum;i++)
+			{
+				for(List<Integer> pre:dp[i-a])
+				{
+					List<Integer> newComb=new ArrayList<>(pre); // TC O(n^2)
+					newComb.add(a);
+					dp[i].add(newComb);
+				}
+			}
+		}
+		for(List<Integer> comb:dp[sum])
+			System.out.println(comb);
+	}
 	public static void main(String[] args) {
 		
 		int coins[] = { 9,6,5,1 };
