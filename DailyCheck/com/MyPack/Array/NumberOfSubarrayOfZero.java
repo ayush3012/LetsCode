1ab06b9c963a18559237e536789eed078809ec39
ayushkarna <k.ayush3012@gmail.com>
2026-05-15 22:18:26 +0530

    number of subarray of zero

--- DailyCheck/com/MyPack/Array/NumberOfSubarrayOfZero.java
+++ DailyCheck/com/MyPack/Array/NumberOfSubarrayOfZero.java
@@ -28,11 +28,32 @@ public class NumberOfSubarrayOfZero {
 		return res;
 	}
 
+	public static int NumberOfSubarray_Method(int[] arr)
+	{
+		int len=0;
+		int count=0;
+        for (int a : arr)
+		{
+            if (a != 0)
+			{
+                count += len * (len + 1) / 2;
+                len = 0;
+            }
+			else
+				len++;
+        }
+		count+=len*(len+1)/2;
+		return count;
+	}
 	public static void main(String[] args) {
 		
 		int arr[] = {0, 0, 0, 0};
+
+		int[] arr2={1,3,0,0,2,0,0,4};
 		
 		System.out.println(NumberOfSubarray(arr));
+
+		System.out.println(NumberOfSubarray_Method(arr));
 
 	}
 
