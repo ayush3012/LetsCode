edf60a7bdb11f68c03b451e755b75ba604d35361
ayushkarna <k.ayush3012@gmail.com>
2026-05-24 22:22:14 +0530

    Interview prep

--- InterviewProgramming/com/example/thread/PrintNumberUsingExecutorFW.java
+++ InterviewProgramming/com/example/thread/PrintNumberUsingExecutorFW.java
@@ -7,27 +7,19 @@ public class PrintNumberUsingExecutorFW {
 
     static int n=1;
     static final int max=100;
-    public static void main(String[] args)
-    {
-        ExecutorService es= Executors.newFixedThreadPool(3);
 
-        Runnable r=()->{
-            while (true)
-            {
-                synchronized (PrintNumberUsingExecutorFW.class)
-                {
-                    while (n<=max)
-                    {
-                        System.out.println(Thread.currentThread().getName()+" "+n);
-                        n++;
-                    }
+    public static void main(String[] args) {
+        ExecutorService es = Executors.newFixedThreadPool(3);
 
-                }
-
+        Runnable r = () -> {
+            while (n <= max) {
+                System.out.println(Thread.currentThread().getName() + " " + n);
+                n++;
             }
+
         };
 
-        for(int i=0;i<3;i++)
+        for (int i = 0; i < 3; i++)
             es.submit(r);
     }
 
