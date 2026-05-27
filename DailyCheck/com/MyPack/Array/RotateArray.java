ab51fc1287ab763bb1a91502ef4fc83a3eb83b45
ayushkarna <k.ayush3012@gmail.com>
2026-05-27 10:18:24 +0530

    rotate array

--- DailyCheck/com/MyPack/Array/RotateArray.java
+++ DailyCheck/com/MyPack/Array/RotateArray.java
@@ -14,15 +14,37 @@ public class RotateArray {
 		return arr;
 	}
 
+	static void reverse(int[] arr,int s,int e)
+	{
+		while (s<e)
+		{
+			int temp=arr[s];
+			arr[s]=arr[e];
+			arr[e]=temp;
+			s++;e--;
+		}
+	}
+
+	static void rotateArrayByK(int[] arr,int k)
+	{
+		k=k%arr.length;
+		reverse(arr,0,arr.length-1);
+		reverse(arr,0,k-1);
+		reverse(arr,k,arr.length-1);
+	}
 	public static void main(String[] args) {
 		
 		
 		int[] arr= {1,2,3,4,5,6,7,8,9};
 		
 		int k=3;
-		arr=RotateArrayByk(arr, k);
+		/*arr=RotateArrayByk(arr, k);
 		for(int i:arr)
-		  System.out.print(i+" ");
+		  System.out.print(i+" ");*/
+
+		rotateArrayByK(arr,2);
+		for (int a:arr)
+			System.out.print(a+" ");
 
 	}
 
