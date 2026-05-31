2bc7aa513b702f22a1539a409576d8c10b1a8dee
ayushkarna <k.ayush3012@gmail.com>
2026-05-31 17:23:45 +0530

    stock buy and sell

--- DailyCheck/com/MyPack/Array/StockBuySell.java
+++ DailyCheck/com/MyPack/Array/StockBuySell.java
@@ -16,11 +16,38 @@ public class StockBuySell {
 		System.out.println("max profit "+max);
 	}
 
+	//If we want to store index
+	static void StockBuySell(int[] arr)
+	{
+		int s=0;
+		int buy=0;
+		int sell=0;
+		int min=arr[0];
+		int profit=Integer.MIN_VALUE;
+		for(int i=1;i<arr.length;i++)
+		{
+			if(min>arr[i])
+			{
+				min=arr[i];
+				s=i;
+			}
+			if(profit<arr[i]-min)
+			{
+				profit=arr[i]-min;
+				buy=s;
+				sell=i;
+			}
+		}
+		System.out.println("for maximum profit buy at "+buy+" and sell at "+sell+" with profit "+profit);
+	}
+
 	public static void main(String[] args) {
 		
 		int price[] = { 100, 180, 260, 310, 40, 535, 695 };
 		
-		StockBuyNSell(price);	
+		StockBuyNSell(price);
+
+		StockBuySell(price);
 
 	}
 
