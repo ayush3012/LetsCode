13fcb1f702862afec7d4b3e3c96b49e6f73572cb
ayushkarna <k.ayush3012@gmail.com>
2026-06-02 17:53:07 +0530

    minimum length subarray

--- DailyCheck/com/MyPack/Array/MinimumLengthSubarrayWithGivenSum.java
+++ DailyCheck/com/MyPack/Array/MinimumLengthSubarrayWithGivenSum.java
@@ -1,5 +1,8 @@
 package com.mypack.Array;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MinimumLengthSubarrayWithGivenSum {
 	
 	public static int subArray(int[] arr,int k)
@@ -18,12 +21,41 @@ public class MinimumLengthSubarrayWithGivenSum {
 		return ans==arr.length+1?0:ans;
 	}
 
+	static void subArrayWithGivenSumMinLen(int[] arr,int k)
+	{
+		Map<Integer,Integer> hm=new HashMap<>();
+		int s=0;
+		int e=0;
+		int window=0;
+		int minLen=Integer.MAX_VALUE;
+		for(int i=0;i<arr.length;i++)
+		{
+			window+=arr[i];
+			if(hm.containsKey(window-k))
+			{
+				int j=hm.get(window-k);
+				if(i-j<minLen)
+				{
+					minLen=i-j;
+					s=j+1;
+					e=i;
+				}
+			}
+			hm.put(window,i);
+		}
+		System.out.println("Minimum length subarray with sum: "+k+" found from "+s+" to "+e);
+	}
+
 	public static void main(String[] args) {
 		
 		int[] arr= {2,4,6,10,2,1};
 		int k=12;
 
 		System.out.println(subArray(arr, k));
+
+		int[] arr2= {2,4,6,10,2,1,0,12};
+		int k2=12;
+		subArrayWithGivenSumMinLen(arr2,k2);
 	}
 
 }
