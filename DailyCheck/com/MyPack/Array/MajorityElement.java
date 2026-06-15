bea7112f5c1c1d7606632f6ade73d0c14b5fefe7
ayushkarna <k.ayush3012@gmail.com>
2026-06-15 22:22:33 +0530

    majority element in minimum jump

--- DailyCheck/com/MyPack/Array/MajorityElement.java
+++ DailyCheck/com/MyPack/Array/MajorityElement.java
@@ -22,11 +22,31 @@ public class MajorityElement {
 		}
 		return arr[ind];
 	}
+
+	// element appear more than n/2
+	static void MajorityElementInMinimumJump(int[] arr)
+	{
+		int count=0;
+		int candidate=0;
+		for(int i:arr)
+		{
+			if(count==0)
+				candidate=i;
+			count+=candidate==i?1:-1;
+		}
+		System.out.println("count of element appear:"+candidate);
+	}
 	public static void main(String[] args) {
 		
         int[] arr= {1,2,8,7,2,6,5};   
 		
 		System.out.println(major(arr));
+
+		System.out.println();
+
+		int[] arr1= {9,9,8,7,2,9,9};
+
+		MajorityElementInMinimumJump(arr1);
 		
 	}
 
