ae55374e0100a3273b9844db8477b368b37c0ba4
ayushkarna <k.ayush3012@gmail.com>
2026-06-21 09:31:11 +0530

    leetcode

--- InterviewProgramming/com/interview/codes/LongestIncreasingSubsequence.java
+++ InterviewProgramming/com/interview/codes/LongestIncreasingSubsequence.java
@@ -1,5 +1,8 @@
 package com.interview.codes;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class LongestIncreasingSubsequence {
 
 	public static int Subsequence(int[] arr)
@@ -21,12 +24,34 @@ public class LongestIncreasingSubsequence {
 		}
 		return max;
 	}
-	
+	static void subsequence_efficient(int[] arr)
+	{
+		Set<Integer> hs=new HashSet<>();
+		int max=0;
+		for(int i:arr) hs.add(i);
+		for(int i:arr)
+		{
+			if(!hs.contains(i-1)) {
+				int curr = i;
+				int len = 1;
+				while (hs.contains(curr + 1)) {
+					curr++;
+					len++;
+				}
+				max = Math.max(len, max);
+			}
+		}
+		System.out.println("max len:"+max);
+	}
 	public static void main(String[] args) {
 		
 		int[] arr= {5,3,4,7,-2,8};
 
 		System.out.println(Subsequence(arr));
+
+		System.out.println();
+
+		subsequence_efficient(arr);
 	}
 
 }
