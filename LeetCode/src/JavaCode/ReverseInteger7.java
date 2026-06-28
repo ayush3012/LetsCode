275d35978a7bb7818a0dd81144e45c3e9a90a59f
ayushkarna <k.ayush3012@gmail.com>
2026-06-28 21:19:29 +0530

    reverse integer

---
+++ LeetCode/src/JavaCode/ReverseInteger7.java
@@ -0,0 +1,23 @@
+package LeetCode.src.JavaCode;
+
+public class ReverseInteger7 {
+
+    public static int reverse(int x) {
+        int num=0;
+        while (x!=0)
+        {
+            int digit=x%10;
+            if(num>Integer.MAX_VALUE/10 || (num==Integer.MAX_VALUE/10 && digit>7))
+                return 0;
+            if(num<Integer.MIN_VALUE/10 || (num==Integer.MIN_VALUE/10 && digit<-8))
+                return 0;
+            num=num*10+digit;
+            x=x/10;
+        }
+        return num;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverse(-321));
+    }
+}
