2717635185bd30538601fa932fc632b63074227a
ayushkarna <k.ayush3012@gmail.com>
2026-06-30 20:50:40 +0530

    climb stairs with dp

--- LeetCode/src/JavaCode/ClimbStairs.java
+++ LeetCode/src/JavaCode/ClimbStairs.java
@@ -27,9 +27,21 @@ public class ClimbStairs {
 		return b;
 	}
 
+	static void DP_impl(int n)
+	{
+		int[] dp=new int[n+1];
+		dp[1]=1;
+		dp[2]=2;
+		for(int i=3;i<=n;i++)
+			dp[i]=dp[i-1]+dp[i-2];
+		System.out.println(dp[n]);
+	}
+
 	public static void main(String[] args) {
 
 		System.out.println(climbStairs(3));
+
+		DP_impl(3);
 	}
 
 }
