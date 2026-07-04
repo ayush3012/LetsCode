2e96431981c0c62c24d9f996d2aed53c9f1e7067
ayushkarna <k.ayush3012@gmail.com>
2026-07-04 21:26:13 +0530

    egg drop

--- DailyCheck/com/MyPack/TwoDArray/EggDrop.java
+++ DailyCheck/com/MyPack/TwoDArray/EggDrop.java
@@ -1,5 +1,7 @@
 package DailyCheck.com.MyPack.TwoDArray;
 
+import java.util.Arrays;
+
 public class EggDrop {
 
     static void eggDropPuzzle(int egg,int floor)
@@ -15,6 +17,32 @@ public class EggDrop {
         }
         System.out.println(moves);
     }
+
+    static int secondApproach(int egg,int floor,int[][] dp)
+    {
+        if(floor==0 || floor==1)
+            return floor;
+        if(egg==1)
+            return floor;
+        if(dp[egg][floor]!=-1)
+            return dp[egg][floor];
+
+        int l=1,h=floor,ans=Integer.MAX_VALUE;
+        while (l<=h)
+        {
+            int mid=(l+h)/2;
+            int breakCase=dp[egg-1][mid-1]!=-1?dp[egg-1][mid-1]:secondApproach(egg-1,mid-1,dp);
+            int notBreakCase=dp[egg][floor-mid]!=-1?dp[egg][floor-mid]:secondApproach(egg,floor-mid,dp);
+
+            int worst=1+Math.max(breakCase,notBreakCase);
+
+            ans=Math.min(ans,worst);
+
+            if(breakCase<notBreakCase) l=mid+1;
+            else h=mid-1;
+        }
+        return dp[egg][floor]=ans;
+    }
     public static void main(String[] args) {
 
         int egg=2;
@@ -22,5 +50,11 @@ public class EggDrop {
 
         eggDropPuzzle(egg,floor);
 
+        int[][] dp=new int[egg+1][floor+1];
+
+        for(int i=0;i<=egg;i++)
+            Arrays.fill(dp[i],-1);
+
+        System.out.println(secondApproach(egg,floor,dp));
     }
 }
