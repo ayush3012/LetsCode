6fe9672b990e3cb1ef94ee93c19beeed30d65a84
ayushkarna <k.ayush3012@gmail.com>
2026-07-08 13:50:22 +0530

    repeat

--- DailyCheck/com/MyPack/String/ReverseBetweenParenthesis1190.java
+++ DailyCheck/com/MyPack/String/ReverseBetweenParenthesis1190.java
@@ -24,9 +24,42 @@ public class ReverseBetweenParenthesis1190 {
         System.out.println(sb);
     }
 
+    static void decodeString(String s)
+    {
+        Stack<Integer> numStack=new Stack<>();
+        Stack<StringBuilder> strStack=new Stack<>();
+        StringBuilder sb=new StringBuilder();
+        int num=0;
+        for(int i=0;i<s.length();i++)
+        {
+            char c=s.charAt(i);
+            if(Character.isDigit(c))
+                num=num*10+(c-'0');
+            else if(c=='[')
+            {
+                numStack.push(num);
+                strStack.push(sb);
+                num=0;
+                sb=new StringBuilder();
+            }
+            else if(c==']')
+            {
+                int a=numStack.pop();
+                StringBuilder x=strStack.pop();
+                for(int j=0;j<a;j++)
+                    x.append(sb);
+                sb=x;
+            }else sb.append(c);
+        }
+        System.out.println(sb);
+    }
     public static void main(String[] args) {
         String s = "(ed(et(oc))el)";
 
         reverse(s);
+
+        String s1="3[a2[b3[c]]]";
+
+        decodeString(s1);
     }
 }
