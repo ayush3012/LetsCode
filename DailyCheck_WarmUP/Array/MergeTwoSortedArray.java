a0b919dbbdc55e12af8ba1153dcd644775ba08f6
ayushkarna <k.ayush3012@gmail.com>
2026-07-13 10:02:09 +0530

    merge two sorted array

--- DailyCheck_WarmUP/Array/MergeTwoSortedArray.java
+++ DailyCheck_WarmUP/Array/MergeTwoSortedArray.java
@@ -1,5 +1,7 @@
 package DailyCheck_WarmUP.Array;
 
+import java.util.Arrays;
+
 public class MergeTwoSortedArray {
 	
 	public static void Merge(int[] arr1,int m,int[] arr2,int n)
@@ -16,12 +18,27 @@ public class MergeTwoSortedArray {
 			System.out.print(i+" ");
 	}
 
+	public static void merge_NoExtraSpace(int[] arr3,int[] arr4)
+	{
+		int i=arr3.length-arr4.length,j=arr4.length,k=arr3.length;
+		while(i>0 && j>0) arr3[--k]=arr3[i-1]>arr4[j-1]?arr3[--i]:arr4[--j];
+		while (i>0) arr3[--k]=arr3[--i];
+		while (j>0) arr3[--k]=arr4[--j];
+
+		System.out.println(Arrays.toString(arr3));
+	}
 	public static void main(String[] args) {
 		
 		int[] arr1= {1,4,6,7};
 		int[] arr2= {1,2,7,9};
+
+		int[] arr3= {1,4,6,7,0,0,0,0};
+		int[] arr4= {1,2,7,9};
 		
 		Merge(arr1, arr1.length, arr2, arr2.length);
+
+		System.out.println();
+		merge_NoExtraSpace(arr3,arr4);
 		
 	}
 
