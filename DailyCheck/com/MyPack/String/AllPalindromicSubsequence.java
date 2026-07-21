2b6879ec9d9d1d59c0e10d13fbdc35b0535b2280
ayushkarna <k.ayush3012@gmail.com>
2026-07-21 21:12:52 +0530

    all palindromic subsequence

--- DailyCheck/com/MyPack/String/AllPalindromicSubsequence.java
+++ DailyCheck/com/MyPack/String/AllPalindromicSubsequence.java
@@ -22,6 +22,27 @@ public class AllPalindromicSubsequence {
         curr.deleteCharAt(curr.length()-1);
         generateAllPalindromicSubsequence(s,ind+1,curr,res);
     }
+
+    static int countPalindromicSubsequence(String s) // for leetcode, if we want only count
+    {
+        int n=s.length();
+        long[][] dp=new long[n][n];
+
+        for(int i=0;i<n;i++)
+            dp[i][i]=1;
+        for(int len=2;len<=n;len++)
+        {
+            for(int i=0;i<=n-len;i++)
+            {
+                int j=i+len-1;
+                if(s.charAt(i)==s.charAt(j))
+                    dp[i][j]=dp[i+1][j]+dp[i][j-1]+1;
+                else
+                    dp[i][j]=dp[i+1][j]+dp[i][j-1]-dp[i+1][j-1];
+            }
+        }
+        return (int)dp[0][n-1];
+    }
     public static void main(String[] args) {
         String s="aba";
 
@@ -30,5 +51,7 @@ public class AllPalindromicSubsequence {
         generateAllPalindromicSubsequence(s,0,new StringBuilder(),res);
 
         System.out.println(res);
+
+        System.out.println(countPalindromicSubsequence(s));
     }
 }
