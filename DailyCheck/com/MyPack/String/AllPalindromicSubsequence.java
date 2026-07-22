e4a92422bf36b6a2aee641ad23d835bc94d64481
ayushkarna <k.ayush3012@gmail.com>
2026-07-22 20:19:32 +0530

    count all palindromic subsequence

--- DailyCheck/com/MyPack/String/AllPalindromicSubsequence.java
+++ DailyCheck/com/MyPack/String/AllPalindromicSubsequence.java
@@ -25,23 +25,55 @@ public class AllPalindromicSubsequence {
 
     static int countPalindromicSubsequence(String s) // for leetcode, if we want only count
     {
-        int n=s.length();
-        long[][] dp=new long[n][n];
+        int n = s.length();
+        long MOD = 1000000007L;
 
-        for(int i=0;i<n;i++)
-            dp[i][i]=1;
-        for(int len=2;len<=n;len++)
-        {
-            for(int i=0;i<=n-len;i++)
-            {
-                int j=i+len-1;
-                if(s.charAt(i)==s.charAt(j))
-                    dp[i][j]=dp[i+1][j]+dp[i][j-1]+1;
-                else
-                    dp[i][j]=dp[i+1][j]+dp[i][j-1]-dp[i+1][j-1];
+        long[][] dp = new long[n][n];
+
+        // Base case
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 1;
+        }
+
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i + len - 1 < n; i++) {
+                int j = i + len - 1;
+
+                if (s.charAt(i) == s.charAt(j)) {
+
+                    int low = i + 1;
+                    int high = j - 1;
+
+                    while (low <= high && s.charAt(low) != s.charAt(i))
+                        low++;
+
+                    while (low <= high && s.charAt(high) != s.charAt(i))
+                        high--;
+
+                    if (low > high) {
+                        // No same character inside
+                        dp[i][j] = dp[i + 1][j - 1] * 2 + 2;
+                    } else if (low == high) {
+                        // One same character inside
+                        dp[i][j] = dp[i + 1][j - 1] * 2 + 1;
+                    } else {
+                        // More than one same character inside
+                        dp[i][j] = dp[i + 1][j - 1] * 2 - dp[low + 1][high - 1];
+                    }
+
+                } else {
+                    dp[i][j] = dp[i + 1][j]
+                            + dp[i][j - 1]
+                            - dp[i + 1][j - 1];
+                }
+
+                dp[i][j] %= MOD;
+                if (dp[i][j] < 0)
+                    dp[i][j] += MOD;
             }
         }
-        return (int)dp[0][n-1];
+
+        return (int) dp[0][n - 1];
     }
     public static void main(String[] args) {
         String s="aba";
