26f1a269eb20ae4e3d549958fccf6a631bf4121d
ayushkarna <k.ayush3012@gmail.com>
2026-08-10 21:28:04 +0530

    leetcode 373

--- DailyCheck/com/MyPack/Array/KSmallestPairsSum373.java
+++ DailyCheck/com/MyPack/Array/KSmallestPairsSum373.java
@@ -10,15 +10,19 @@ public class KSmallestPairsSum373 {
     static void KSmallestPairSum(int[] nums1,int[] nums2,int k)
     {
         List<List<Integer>> res=new ArrayList<>();
+
         PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->
                 Integer.compare(nums1[a[0]]+nums2[a[1]],nums1[b[0]]+nums2[b[1]]));
 
-        for(int i=0;i<nums1.length && i<k;i++)
+        for(int i=0;i<nums1.length;i++)
             pq.add(new int[]{i,0});
-        while (k-->0 && !pq.isEmpty())
+
+        while(k-->0)
         {
             int[] curr=pq.poll();
+
             res.add(Arrays.asList(nums1[curr[0]],nums2[curr[1]]));
+
             if(curr[1]+1<nums2.length)
                 pq.add(new int[]{curr[0],1+curr[1]});
         }
