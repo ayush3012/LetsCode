63974de3ed723b510778cdb1796cc78d33575a62
ayushkarna <k.ayush3012@gmail.com>
2026-08-20 17:52:58 +0530

    leetcode 45

--- DailyCheck_WarmUP/Array/CanJump45.java
+++ DailyCheck_WarmUP/Array/CanJump45.java
@@ -1,5 +1,7 @@
 package DailyCheck_WarmUP.Array;
 
+import java.util.Arrays;
+
 public class CanJump45 {
 
     static void minJump(int[] nums)
@@ -18,6 +20,19 @@ public class CanJump45 {
             }
         }
         System.out.println(jump);
+    }
+
+    public static int jump(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 0; i < nums.length; i++)
+        {
+            for (int j = i + 1; j <= Math.min(i + nums[i], nums.length - 1); j++)
+                if (dp[i] != Integer.MAX_VALUE)
+                    dp[j] = Math.min(dp[j], dp[i] + 1);
+        }
+        return dp[nums.length - 1];
     }
     public static void main(String[] args) {
         int[] nums={2,3,0,1,4};
