dd67b269f5b6997ebdf3d89fe3fc76f69953e464
ayushkarna <k.ayush3012@gmail.com>
2026-08-23 11:51:17 +0530

    deadlock

--- InterviewProgramming/com/example/thread/DeadLock.java
+++ InterviewProgramming/com/example/thread/DeadLock.java
@@ -1,5 +1,9 @@
 package InterviewProgramming.com.example.thread;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
+
 /**
  * Deadlock can happen :
  * 1. mutual exclusion:- resource can be used by one thread at a time. e.g synchronized block
@@ -17,16 +21,16 @@ public class DeadLock {
     {
         Thread t1=new Thread(()->{
                 synchronized (lock1) {
-                    System.out.println("thread1 acquired lock1");
+                    System.out.println(Thread.currentThread().getName()+" acquired lock1");
                     try {
                         Thread.sleep(100);
                     } catch (InterruptedException e) {
                     }
 
-                    System.out.println("thread1 waiting for lock2");
+                    System.out.println(Thread.currentThread().getName()+" waiting for lock2");
 
                     synchronized (lock2) {
-                        System.out.println("thread1 acquired lock2");
+                        System.out.println(Thread.currentThread().getName()+" acquired lock2");
                     }
                 } //synchronized1 should end here
 
@@ -34,16 +38,16 @@ public class DeadLock {
 
         Thread t2=new Thread(()->{
             synchronized (lock2) {
-                System.out.println("thread2 acquired lock2");
+                System.out.println(Thread.currentThread().getName()+" acquired lock2");
                 try {
                     Thread.sleep(100);
                 } catch (InterruptedException e) {
                 }
 
-                System.out.println("thread2 waiting for lock1");
+                System.out.println(Thread.currentThread().getName()+" waiting for lock1");
 
                 synchronized (lock1) {
-                    System.out.println("thread2 acquired lock1");
+                    System.out.println(Thread.currentThread().getName()+" acquired lock1");
                 }
 
             }
@@ -52,5 +56,25 @@ public class DeadLock {
 
         t1.start();
         t2.start();
+
+
+        try {
+            Thread.sleep(500);
+        }catch (Exception e){}
+
+        ThreadMXBean bean= ManagementFactory.getThreadMXBean();
+        long[] dlt=bean.findDeadlockedThreads();
+
+        if(dlt!=null)
+        {
+            System.out.println("deadlock found between following threads..");
+            for(long id:dlt)
+            {
+                ThreadInfo info=bean.getThreadInfo(id);
+                System.out.println(info.getThreadName());
+            }
+        }else {
+            System.out.println("no deadlock");
+        }
     }
 }
