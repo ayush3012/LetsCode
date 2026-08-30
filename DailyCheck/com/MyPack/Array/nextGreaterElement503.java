209d42e0e165f6a8a7e235c70e819f66ca21a385
ayushkarna <k.ayush3012@gmail.com>
2026-08-30 11:10:31 +0530

    leetcode 503

--- DailyCheck/com/MyPack/Array/nextGreaterElement503.java
+++ DailyCheck/com/MyPack/Array/nextGreaterElement503.java
@@ -23,6 +23,30 @@ public class nextGreaterElement503 {
         return res;
     }
 
+    public int[] nextGreaterElements_mySolution(int[] nums) {
+        int[] res=new int[nums.length];
+        Stack<Integer> stk=new Stack<>();
+        for(int i=nums.length-1;i>=0;i--)
+        {
+            int j=i;
+            int n=0;
+            while (nums[j% nums.length]<=nums[i])
+            {
+                j++;n++;
+                if(n==nums.length)
+                {
+                    n=0;
+                    break;
+                }
+            }
+            while(!stk.isEmpty() && stk.peek()<=nums[i])
+                stk.pop();
+            res[i]=!stk.isEmpty()?stk.peek():(n!=0?nums[j% nums.length]:-1);
+            stk.push(nums[i]);
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         int[] nums={1,2,1};
 
